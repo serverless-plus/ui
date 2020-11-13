1cf6d8b0f69a8565b8260110a3b6237a8b57c355
yugasun <yuga_sun@163.com>
2020-11-13 14:54:08 +0800

    fix: start url

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ import findPort from './utils/find-port';
 async function startServer(route = '/'): Promise<Server> {
   const PORT = await findPort(process.env.PORT as string);
   const server = app.listen(PORT, async () => {
-    const url = `http://localhost:${PORT}$`;
+    const url = `http://localhost:${PORT}`;
     const openUrl = `${url}${route}`;
     console.log(`  App is running at ${url} in ${app.get('env')} mode`);
     try {
